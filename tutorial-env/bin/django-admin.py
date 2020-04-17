c407cef92c594832f11e8a24b00e68284157347f
Ivica <ivica-matic@outlook.com>
2020-04-17 15:09:46 +0100

    Removed env

--- tutorial-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ivica/Coding/Notts-Police-Comunity-Engagement-Management/tutorial-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
