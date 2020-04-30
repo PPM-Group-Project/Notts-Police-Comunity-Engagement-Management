b305634cee81ed111dec038c08d8381db8728962
Ivica <ivica-matic@outlook.com>
2020-04-30 22:41:07 +0100

    Added logo and favicon

--- cepf/cepf/settings.py
+++ cepf/cepf/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'management',
     'community' ,
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
